3c7871ca6a7b5ded090ecb62996bb4f28c211950
Wingpan <wind-wing@126.com>
2017-01-18 16:18:05 +0800

    fix

--- src/com/youguo/demo/consistentHashing/AppMain.java
+++ src/com/youguo/demo/consistentHashing/AppMain.java
@@ -8,16 +8,30 @@ import java.util.Set;
 public class AppMain {
     public static void main(String[] args) {
         Shard shard = new Shard(generateNodes());
+        int totalCount = 100000;
         Random random = new Random();
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < totalCount; i++) {
             shard.add(random.nextInt());
         }
 
         Set<Node> nodes = shard.getNodes();
+        int max = 0;
+        int min = totalCount;
         for (Node node : nodes) {
-            System.out.println(node + ": " + node.cachedCount());
+            int nodeCacheCount = node.cachedCount();
+            System.out.println(node + ": " + nodeCacheCount);
             System.out.println("++++++++++++++++++++++++++++++");
+
+            max = Math.max(max, nodeCacheCount);
+            min = Math.min(min, nodeCacheCount);
         }
+
+        System.out.println("*******************************");
+        int Avg = totalCount/nodes.size();
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
+        System.out.println("AVG: " + Avg);
+
     }
 
     private static Set<Node> generateNodes() {
